9a6729fbf44921cc4248e3a720cfb79c2370909b
Forsomethings1 <145438643+AzurePLUA@users.noreply.github.com>
2025-12-01 19:39:12 -0600

    Added correct Target designator catagory

--- lua/entities/target_designator_screen.lua
+++ lua/entities/target_designator_screen.lua
@@ -6,16 +6,15 @@ DEFINE_BASECLASS("base_gmodentity")
 
 ENT.Type = "anim"
 ENT.Base = "base_gmodentity"
-ENT.PrintName = "Screen Entity"
+ENT.PrintName = "Target Designator Screen"
 ENT.Author = "YourName"
-ENT.Category = "G-missiles"
+ENT.Category = "G-missiles Targeting"
 ENT.Spawnable = true
 
 if SERVER then
-
     -- Store the server start time
     local serverStartTime = CurTime()
-    
+
     function ENT:Initialize()
         self:SetModel("models/hunter/plates/plate2x2.mdl")
         self:SetMaterial("models/debug/debugwhite")
@@ -31,31 +30,29 @@ if SERVER then
         self:FindEntitiesInSphere()
         self.TypingText = "ACQUIRING TARGETS...."
         self.TypingProgress = 0
-        self.TypingSpeed = 50 -- Characters per second
-       
+        self.TypingSpeed = 50                    -- Characters per second
+
         self.ButtonOffset1 = Vector(10, -20, 0)  -- Offset for button 1
-        self.ButtonOffset2 = Vector(-10, -20, 0)  -- Offset for button 2
-        
+        self.ButtonOffset2 = Vector(-10, -20, 0) -- Offset for button 2
+
 
 
         self:CreateButtons()
-
     end
 
     function ENT:CreateButtons()
-        
-            local button1 = ents.Create("target_designator_screen_button1")
-            if IsValid(button1) then
-                button1:SetPos(self:GetPos() + self.ButtonOffset1)
-                button1:SetParent(self)
-                button1:Spawn()
-            end
-            local button2 = ents.Create("target_designator_screen_button2")
-            if IsValid(button2) then
-                button2:SetPos(self:GetPos() + self.ButtonOffset2)
-                button2:SetParent(self)
-                button2:Spawn()
-            end
+        local button1 = ents.Create("target_designator_screen_button1")
+        if IsValid(button1) then
+            button1:SetPos(self:GetPos() + self.ButtonOffset1)
+            button1:SetParent(self)
+            button1:Spawn()
+        end
+        local button2 = ents.Create("target_designator_screen_button2")
+        if IsValid(button2) then
+            button2:SetPos(self:GetPos() + self.ButtonOffset2)
+            button2:SetParent(self)
+            button2:Spawn()
+        end
     end
 
     function ENT:UpdateTypingText()
@@ -73,8 +70,6 @@ if SERVER then
         self:NextThink(CurTime() + .1) -- Adjust the update rate as needed
         return true
     end
-
-    
 
     function ENT:FindEntitiesInSphere()
         local pos = self:GetPos()
@@ -131,7 +126,7 @@ if SERVER then
         -- Reset color of the previous entity
         local previousEntity = self.EntitiesList[self.CurrentIndex]
         if IsValid(previousEntity) then
-        previousEntity:SetColor(Color(255, 255, 255)) -- Reset to white or original color
+            previousEntity:SetColor(Color(255, 255, 255)) -- Reset to white or original color
         end
 
         self.CurrentIndex = self.CurrentIndex + direction
@@ -147,7 +142,7 @@ if SERVER then
             if currentEntity:IsPlayer() then
                 self:SetNWString("DisplayText", currentEntity:Nick())
             else
-                self:SetNWString("DisplayText", currentEntity:GetClass())   
+                self:SetNWString("DisplayText", currentEntity:GetClass())
             end
 
             self:SetNWEntity("NW_Target", currentEntity)
@@ -157,50 +152,46 @@ if SERVER then
             self:SetNWString("DisplayText", "No Entities Found")
             self:SetNWBool("TargetMarked", false)
         end
-
-
-        
     end
 
     function ENT:Use(activator, caller)
         -- Placeholder for button interaction; logic handled in button entity
     end
-
-
-else-- (CLIENT SIDE)
+else -- (CLIENT SIDE)
     function ENT:Draw()
         self:DrawModel()
-        
+
         local pos = self:GetPos() + self:GetUp() * 1.8
         local ang = self:GetAngles()
         local Box = draw.RoundedBox;
 
-        
+
         ang:RotateAroundAxis(ang:Forward(), 0)
         ang:RotateAroundAxis(ang:Right(), 0)
 
         cam.Start3D2D(pos, ang, 0.2)
-            Box(10, -230, -230, 460, 460, Color(0, 0, 0));
-            --Box(0, 60, -25, 123, 20, Color(0, 0, 0));
-            draw.SimpleText(self:GetNWString("TypingText", "ACQUIRING TARGETS"), "DermaLarge", 0, -190, Color(255, 255, 255, 255), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
-            draw.SimpleText("CURRENT TARGET -", "DermaLarge", 0, -90, Color(255, 255, 255), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
-            draw.SimpleText(self:GetNWString("DisplayText", "No Entities Found"), "TargetId", 0, 0, Color(255, 0, 0), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+        Box(10, -230, -230, 460, 460, Color(0, 0, 0));
+        --Box(0, 60, -25, 123, 20, Color(0, 0, 0));
+        draw.SimpleText(self:GetNWString("TypingText", "ACQUIRING TARGETS"), "DermaLarge", 0, -190,
+            Color(255, 255, 255, 255), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+        draw.SimpleText("CURRENT TARGET -", "DermaLarge", 0, -90, Color(255, 255, 255), TEXT_ALIGN_CENTER,
+            TEXT_ALIGN_CENTER)
+        draw.SimpleText(self:GetNWString("DisplayText", "No Entities Found"), "TargetId", 0, 0, Color(255, 0, 0),
+            TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
         cam.End3D2D()
     end
-end   
+end
 
 
-function ENT:SpawnFunction( ply, tr )-- Used so the ENT doesnt fucking spawn in the ground
-	
-        if ( not tr.Hit ) then return end
-         self.GBOWNER = ply
-         local ent = ents.Create( self.ClassName )
-         ent:SetPhysicsAttacker(ply)
-         ent:SetPos( tr.HitPos + tr.HitNormal * 4 ) -- Changing the right most number makes the entity spawn further up or down...
-         ent:Spawn()
-         ent:SetAngles(Angle(-90,-90,-90))
-         ent:Activate()
-         
-         return ent
-         
+function ENT:SpawnFunction(ply, tr) -- Used so the ENT doesnt fucking spawn in the ground
+    if (not tr.Hit) then return end
+    self.GBOWNER = ply
+    local ent = ents.Create(self.ClassName)
+    ent:SetPhysicsAttacker(ply)
+    ent:SetPos(tr.HitPos + tr.HitNormal * 4) -- Changing the right most number makes the entity spawn further up or down...
+    ent:Spawn()
+    ent:SetAngles(Angle(-90, -90, -90))
+    ent:Activate()
+
+    return ent
 end
