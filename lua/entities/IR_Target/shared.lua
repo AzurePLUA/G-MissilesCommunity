b0da2d93c0f4d2d19093333b418da9bfa7a2bee6
Forsomethings1 <145438643+AzurePLUA@users.noreply.github.com>
2025-12-04 13:04:04 -0600

    Ir target moved to targeting catagory

--- lua/entities/IR_Target/shared.lua
+++ lua/entities/IR_Target/shared.lua
@@ -1,14 +1,13 @@
- 
 ENT.Type = "anim"
 ENT.Base = "base_anim"
- 
 
-ENT.Category       		= "G-Missiles Guided"
-ENT.PrintName			= "G-IR Target" 
-ENT.Author			= "Forsomethings1"
-ENT.Contact			= "Don't"
-ENT.Purpose			= "Missiles can target this"
-ENT.Instructions		= ""
- 
-ENT.Spawnable 			= true
-ENT.ArmSound                         =  "GMissiles/arm/hominglockaquired1.wav" 
+
+ENT.Category     = "G-missiles Targeting "
+ENT.PrintName    = "G-IR Target"
+ENT.Author       = "Forsomethings1"
+ENT.Contact      = "Don't"
+ENT.Purpose      = "Missiles can target this"
+ENT.Instructions = ""
+
+ENT.Spawnable    = true
+ENT.ArmSound     = "GMissiles/arm/hominglockaquired1.wav"
